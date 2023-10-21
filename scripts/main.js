a102b551867ab181066fa5d7da21b9eae95187a4
Benfill <Anasse.benfillous@gmail.com>
2023-10-21 18:10:09 +0100

    fixing dark mode with refresh

--- scripts/main.js
+++ scripts/main.js
@@ -15,33 +15,51 @@ const darkMode = document.querySelector(".js-dark");
 const news = document.querySelector(".js-news");
 const sectionTitle = document.querySelector(".js-section-title");
 const sectionTitle2 = document.querySelector(".js-section-title2");
-checker = 0;
-checker = Number(localStorage.getItem("mode"));
-console.log(checker);
 
-if (checker === 0) {
-  checker = 1;
-  changeMode();
-  localStorage.setItem("mode", String(checker));
+let checker = localStorage.getItem("mode");
+
+if (checker === "on") {
+  darkMode.classList.toggle("lightOff");
+  lightMode.classList.toggle("lightOff");
+  menuLight.classList.toggle("menuOff");
+  menuDark.classList.toggle("menuOff");
+  bellLight.classList.toggle("bellOff");
+  bellDark.classList.toggle("bellOff");
+  body.classList.toggle("body-light");
+  header.classList.toggle("header-light");
+  login.classList.toggle("login-light");
+  loginDark.classList.toggle("login-light");
+  news.classList.toggle("newsLight");
+  sectionTitle.classList.toggle("sectionTitleOff");
+  sectionTitle2.classList.toggle("sectionTitleOff");
+  navList.classList.toggle("nav-list-mode");
 }
 
 function changeMode() {
-    darkMode.classList.toggle("lightOff");
-    lightMode.classList.toggle("lightOff");
-    menuLight.classList.toggle("menuOff");
-    menuDark.classList.toggle("menuOff");
-    bellLight.classList.toggle("bellOff");
-    bellDark.classList.toggle("bellOff");
-    body.classList.toggle("body-light");
-    header.classList.toggle("header-light");
-    login.classList.toggle("login-light");
-    loginDark.classList.toggle("login-light");
-    news.classList.toggle("newsLight");
-    sectionTitle.classList.toggle("sectionTitleOff");
-    sectionTitle2.classList.toggle("sectionTitleOff");
-    navList.classList.toggle("nav-list-mode");
-    checker = 0;
-    localStorage.setItem("mode", String(checker)); 
+  darkMode.classList.toggle("lightOff");
+  lightMode.classList.toggle("lightOff");
+  menuLight.classList.toggle("menuOff");
+  menuDark.classList.toggle("menuOff");
+  bellLight.classList.toggle("bellOff");
+  bellDark.classList.toggle("bellOff");
+  body.classList.toggle("body-light");
+  header.classList.toggle("header-light");
+  login.classList.toggle("login-light");
+  loginDark.classList.toggle("login-light");
+  news.classList.toggle("newsLight");
+  sectionTitle.classList.toggle("sectionTitleOff");
+  sectionTitle2.classList.toggle("sectionTitleOff");
+  navList.classList.toggle("nav-list-mode");
+
+  savingMode();
+}
+
+function savingMode() {
+  if (checker === "on") {
+    localStorage.setItem("mode", "off");
+  } else {
+    localStorage.setItem("mode", "on");
+  }
 }
 
 function toggleNav() {
